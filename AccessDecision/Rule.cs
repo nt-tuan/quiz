e576dd27159e6b216a988c08ad1dfef0fa8e772e
Nguyen Thanh Tuan <thanhtuan12t@gmail.com>
2020-11-10 19:23:35 +0700

    fix array methods

--- AccessDecision/Rule.cs
+++ AccessDecision/Rule.cs
@@ -1,15 +1,16 @@
 using System;
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 namespace dmc_auth.AccessDecision
 {
-    public class Rule
-    {
-        [JsonPropertyName("path")]
-        public string Path { get; set; }
-        [JsonPropertyName("role")]
-        public string Role { get; set; }
-        [JsonPropertyName("methods")]
-        public string Methods { get; set; }
-    }
+  public class Rule
+  {
+    [JsonPropertyName("path")]
+    public string Path { get; set; }
+    [JsonPropertyName("role")]
+    public string Role { get; set; }
+    [JsonPropertyName("methods")]
+    public List<string> Methods { get; set; }
+  }
 }
