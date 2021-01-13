50840ab98dfad1b63382fd03aa3be9c055eac501
Nguyen Thanh Tuan <thanhtuan12t@gmail.com>
2021-01-13 16:19:02 +0700

    fix authorize

--- Controllers/AuthorizeController.cs
+++ Controllers/AuthorizeController.cs
@@ -30,21 +30,21 @@ namespace CleanArchitecture.Web.Api
       var method = Request.Headers["X-Request-Method"];
       var rule = _decision.GetFirstMatchedRule(path, method);
       if (rule == null) return Ok();
-      if (string.IsNullOrEmpty(bearertoken)) return Unauthorized("no-authentication-found");
-      if (bearertoken.Length <= "Bearer".Length) return Unauthorized("no-authentication-found");
+      if (string.IsNullOrEmpty(bearertoken)) return Unauthorized();
+      if (bearertoken.Length <= "Bearer".Length) return Unauthorized();
       var accessToken = bearertoken["Bearer ".Length..];
       try
       {
         var result = await _hydra.IntrospectToken(accessToken, null);
         if (!result.Active)
-          return Unauthorized("invalidated-token");
+          return Unauthorized();
         _logger.LogInformation("Subject {0}, Scope {1}", result.Sub, result.Scope);
         Response.Headers.Add("X-Subject", result.Sub);
         Response.Headers.Add("X-Scope", result.Scope);
         if (string.IsNullOrEmpty(rule.Scope)) return Ok();
         var scope = result.Scope;
         if (!result.Scope.Contains(rule.Scope))
-          return Unauthorized("invalid-scope");
+          return Forbid();
         return Ok();
       }
       catch (Exception e)
