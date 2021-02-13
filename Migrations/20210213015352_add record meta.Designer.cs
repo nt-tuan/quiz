ddf9ec4afb19c68810f2d3f280effc70545800d1
Nguyen Thanh Tuan <thanhtuan12t@gmail.com>
2021-02-13 08:54:38 +0700

    db: add migration

--- Migrations/20210213015352_add record meta.Designer.cs
+++ Migrations/20210213015352_add record meta.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ThanhTuan.Quiz.Data;
@@ -9,9 +10,10 @@ using ThanhTuan.Quiz.Data;
 namespace dmc_auth.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210213015352_add record meta")]
+    partial class addrecordmeta
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20210213015352_add record meta.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dmc_auth.Migrations
+{
+    public partial class addrecordmeta : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "Questions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Questions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeletedAt",
+                table: "Questions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletedBy",
+                table: "Questions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "Exams",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Exams",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeletedAt",
+                table: "Exams",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletedBy",
+                table: "Exams",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "AnswerOptions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "AnswerOptions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeletedAt",
+                table: "AnswerOptions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletedBy",
+                table: "AnswerOptions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Exams");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Exams");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Exams");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                table: "Exams");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "AnswerOptions");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "AnswerOptions");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "AnswerOptions");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                table: "AnswerOptions");
+        }
+    }
+}
