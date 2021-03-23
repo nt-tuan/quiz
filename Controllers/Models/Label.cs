16de8b35c9a4163d52898fc95a0dfc1ff14e65b4
Nguyen Thanh Tuan <thanhtuan12t@gmail.com>
2021-03-23 21:13:47 +0700

    add label id

--- Controllers/Models/Label.cs
+++ Controllers/Models/Label.cs
@@ -4,12 +4,14 @@ namespace ThanhTuan.Quiz.Controllers.Models
 {
   public class Label
   {
+    public int Id { get; set; }
     public string KeyId { get; set; }
     public string Value { get; set; }
     public string DisplayName { get; set; }
     public Label() { }
     public Label(Entities.Label entity)
     {
+      Id = entity.Id;
       KeyId = entity.KeyId;
       Value = entity.Value;
       DisplayName = entity.DisplayName;
@@ -18,6 +20,7 @@ namespace ThanhTuan.Quiz.Controllers.Models
     {
       return new Entities.Label
       {
+        Id = Id,
         KeyId = KeyId,
         Value = Value,
         DisplayName = DisplayName
